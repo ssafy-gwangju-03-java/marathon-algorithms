2fbf25b79117e86caa148e6dd74ddc05f5d4c175
garamgim <garamgim@gmail.com>
2024-06-04 13:00:56 +0900

    [김가람] 1주차 2번 풀이 제출

--- week_01/문제_02/김가람_2224.java
+++ week_01/문제_02/김가람_2224.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class 김가람_2224 {
     static int N;
@@ -14,6 +13,9 @@ public class 김가람_2224 {
         // A, B, C, ... x, y, z (52개)
         // 전건과 후건의 사이의 참거짓 여부를 표시해 줄 행렬
         graph = new boolean[52][52];
+
+        // 참인 명제의 총 갯수
+        int total = 0;
 
         // 입력
         for (int i = 0; i < N; i++) {
@@ -34,29 +36,35 @@ public class 김가람_2224 {
                 }
             }
 
-            graph[indices[0]][indices[1]] = true;
+            // 같은 명제가 여러번 주어질 수 있으므로 total의 갯수를 정확히 하기 위해 중복 체크
+            if (!graph[indices[0]][indices[1]]) {
+                graph[indices[0]][indices[1]] = true;
+                total++;
+            }
         }
+
 
         // 플로이드 워셜
         // k 위치 잘못 설정해서 오답
         // - k는 항상 반복문의 가장 바깥에 위치해야 함
         // - k가 만약 반복문의 가장 안쪽에 있다면 i, j의 연결 여부는 그저 단순하고 순차적으로 갱신될 뿐임
         // 참고 : https://github.com/ssafy-gwangju-03-java/ssafy-gwangju3-java-algorithms/blob/master/week_09_%EB%8B%A4%EC%9D%B5%EC%8A%A4%ED%8A%B8%EB%9D%BC/%EA%B9%80%EA%B0%80%EB%9E%8C/B_1_14938.java
+
         for (int k = 0; k < 52; k++) {
             for (int i = 0; i < 52; i++) {
                 for (int j = 0; j < 52; j++) {
                     if (graph[i][j] || i == j) continue;
-                    graph[i][j] = (graph[i][k] && graph[k][j]);
+                    if ((graph[i][k] && graph[k][j])) {
+                        graph[i][j] = true;
+                        total++;
+                    }
                 }
             }
         }
 
-
-        // 명제로 변환해 담아줄 배열
-        ArrayList<char[]> statements = new ArrayList<>();
+        System.out.println(total);
 
         // 현재 인덱스로 표현된 전건, 후건을 다시 Char로 변경
-        // 52가 아닌 58로 잡았다면 거치치 않아도 되었을 과정 ('Z'와 'a'의 아스키값 차이는 6밖에 안됨...)
         for (int i = 0; i < 52; i++) {
             for (int j = 0; j < 52; j++) {
                 if (graph[i][j]) {
@@ -71,15 +79,9 @@ public class 김가람_2224 {
                         }
                     }
 
-                    statements.add(chars);
+                    System.out.println(chars[0] + " => " + chars[1]);
                 }
             }
-        }
-
-        System.out.println(statements.size());
-
-        for (char[] statement : statements) {
-            System.out.println(statement[0] + " => " + statement[1]);
         }
     }
 }
