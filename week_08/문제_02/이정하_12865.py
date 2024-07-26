4b640f4155d220b7a06ea741601a1204aedfcc9b
이정하 <dlchris19@gmail.com>
2024-07-26 13:30:15 +0900

    0726

--- week_08/문제_02/이정하_12865.py
+++ week_08/문제_02/이정하_12865.py
@@ -4,3 +4,6 @@ N, K = map(int, input().split())
 things = [tuple(map(int, input().split())) for _ in range(N)]
 
 # https://jeonyeohun.tistory.com/86
+
+# bag[k][w] = 물건 번호 k, k번째 물건의 무게 w
+bag = [[0] * (N + 1) for _ in range(W + 1)]
